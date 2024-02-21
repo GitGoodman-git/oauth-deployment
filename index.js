49734245dd3d17b634006a0b79cf8736c6a46127
Sumit Joshi <git.sumitjoshi@gmail.com>
2024-02-21 19:33:45 +0530

    Updated some values

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const  path = require('path');
 
 const  oauth =require(path.join(process.cwd(),"auth","Oauth"));
 
-const handlebars=require('express-handlebars');
+//const handlebars=require('express-handlebars');
 const app=express();
 /*
 app.set('handlebars',handlebars.engine());
@@ -12,13 +12,13 @@ app.set('view engine','handlebars');
 app.set('views','./views');
 app.engine('handlebars');
 */
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 app.use(oauth);
 
-app.use(parser.json());
-app.use(parser.urlencoded({ extended: true }));
+//app.use(parser.json());
+//app.use(parser.urlencoded({ extended: true }));
 app.use('/home',(req,res)=>{
-   res.render("home");
+   res.send("Done")//res.render("home");
 });
 
 app.listen(3000,()=>console.log("started"))
